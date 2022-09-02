0b272053a097e06e4d5a148a90149d724bfcaa02
yeeun-b <yeeun.park.417@gmail.com>
2022-09-02 17:29:24 +0900

    0902 TodoList 페이지에 import

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,7 +1,8 @@
 import React from "react";
 // import useState from "react";
 import './Todolist.css';
-import TodoInsert from './TodoInsert';
+import TodoInsert from './TodoInsert'; // 할 일 추가하는 페이지
+import TodoListList from "./TodoListList"; // 할 일 목록
 
 const TodoList = ({todos}) => {
     return (
@@ -9,6 +10,7 @@ const TodoList = ({todos}) => {
             <div className="Todo-title">TodoList</div>
             <div className="content">{todos}</div>
             <TodoInsert /> {/* 할 일 추가하는 페이지 */}
+            <TodoListList />
         </div>
     );
 };
