18edd7dbeecd98fb55466e26d1a3839efa2f0aca
yeeun-b <yeeun.park.417@gmail.com>
2022-09-02 17:25:57 +0900

    0902 TodoListItem 페이지

---
+++ src/TodoListItem.js
@@ -0,0 +1,18 @@
+import { MdCheckBox, MdCheckBoxOutlineBlank, MdRemoveCircleOutline } from "react-icons/md";
+import './TodoListItem.css';
+
+const TodoListItem = () => {
+    return(
+        <div className="TodoListItem">
+            <div className="checkbox">
+                <MdCheckBoxOutlineBlank />
+                <div className="text">할 일</div>
+            </div>
+            <div className="remove">
+                <MdRemoveCircleOutline />
+            </div>
+        </div>
+    );
+};
+
+export default TodoListItem;
