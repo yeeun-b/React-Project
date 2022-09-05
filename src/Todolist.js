af5ebcad8de5ddb70454e8d93bb30a525d8ddae3
yeeun-b <yeeun.park.417@gmail.com>
2022-09-05 18:05:31 +0900

    0905 useRef, useCallback 사용해 todos배열에 객체 추가하기

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,11 +1,11 @@
 import React from "react";
-import {useState} from "react";
+import {useState, useRef, useCallback} from "react";
 import './Todolist.css';
 import TodoInsert from './TodoInsert'; // 할 일 추가하는 페이지
 import TodoListList from "./TodoListList"; // 할 일 목록
 
 const TodoList = () => {
-    const [todos, seTodos] = useState([
+    const [todos, setTodos] = useState([
         // 할 일 목록
         // 배열이 props로 전달됨
         {
@@ -25,11 +25,27 @@ const TodoList = () => {
         },
     ]);
 
+    // 고유값으로 사용될 id
+    const nextId = useRef(4);
+
+    const onInsert = useCallback(
+        text => {
+            const todo = {
+                id: nextId.current,
+                text,
+                checked:false,
+            };
+            setTodos(todos.concat(todo));
+            nextId.current += 1; // nextId를 1씩 더하기
+        },
+        [todos],
+    );
+
     return (
         <div className="TodoTemplate">
             <div className="Todo-title">TodoList</div>
             {/* <div className="content"> */}
-            <TodoInsert /> {/* 할 일 추가하는 페이지 */}
+            <TodoInsert onInsert={onInsert}/> {/* 할 일 추가하는 페이지 */}
             <TodoListList todos={todos}/> {/* 리스트 출력하는 페이지 */}
             {/* </div> */}
         </div>
