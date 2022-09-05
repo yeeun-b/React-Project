8d2d006749e0c43714715ca53960277006404ae8
yeeun-b <yeeun.park.417@gmail.com>
2022-09-05 17:59:25 +0900

    09/05 useState, useCallback으로 Input설정

--- src/TodoInsert.js
+++ src/TodoInsert.js
@@ -1,11 +1,21 @@
 import './TodoInsert.css';
 import {MdAdd} from 'react-icons/md'; // icon
+import {useCallback, useState} from "react";
 
 // 할 일 추가하는 페이지 & 버튼
 const TodoInsert =()=>{
+    const [value, setValue] = useState(''); // 입력하는 값 관리
+
+    const onChange = useCallback(e => { // input에 넣을 onChange
+        // 함수 재사용하기 위해 useCallback 사용함
+        setValue(e.target.value);
+    }, []);
+
     return(
         <form className='TodoInsert'>
-            <input placeholder='할 일 추가하기' />
+            <input placeholder='할 일 추가하기' 
+                value={value}
+                onChange={onChange}/>
             <button type='submit' className='ButtonTodoInsert'>
                 <MdAdd />
             </button>
