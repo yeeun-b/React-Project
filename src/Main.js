9a048f11bc3ca04fd42623f302e1e1c0d906d716
yeeun-b <yeeun.park.417@gmail.com>
2022-09-07 17:28:35 +0900

    0907 메인페이지 css 생성 및 수정

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,15 @@
 import {Link, useNavigate} from 'react-router-dom'; // 링크 설정
 import {Button} from 'antd';
+import './Main.css';
 
 function Main(){
     // useHistory 생성
     const history = useNavigate();
 
     return(
-        <div>
+        <div className="MainDiv">
             <h3>Main</h3>
-            <Button onClick={() => {
+            <Button className="ButtonTodolist" onClick={() => {
               history('/todolist');
             }} 
               size='large' 
@@ -16,7 +17,7 @@ function Main(){
             >
               Todo
             </Button>
-            <Button onClick={() => {
+            <Button className="ButtonCatdog" onClick={() => {
               history('/catdog');
             }} 
               size='large' 
