d61bec36202cec41a6e52217e98b1bcc3d1228f3
yeeun-b <yeeun.park.417@gmail.com>
2022-09-07 17:36:09 +0900

    0907 할일 삭제하는 함수 호출

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -2,8 +2,8 @@ import { MdCheckBox, MdCheckBoxOutlineBlank, MdRemoveCircleOutline } from "react
 import './TodoListItem.scss';
 import cn from 'classnames';
 
-const TodoListItem = ({todo}) => {
-    const {text, checked} = todo;
+const TodoListItem = ({todo, onRemove}) => {
+    const {id, text, checked} = todo; // id도 추가함
 
     return(
         <div className="TodoListItem">
@@ -11,7 +11,8 @@ const TodoListItem = ({todo}) => {
                 {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
                 <div className="text">{text}</div>
             </div>
-            <div className="remove">
+            <div className="remove" onClick={() => onRemove(id)}>
+                {/* 클릭 시 id를 기준으로 항목 삭제하는 함수 호출 */}
                 <MdRemoveCircleOutline />
             </div>
         </div>
