ff5b6ea65b159f8a174a43c409046196f8ed0bc8
yeeun-b <yeeun.park.417@gmail.com>
2022-09-07 18:28:42 +0900

    0907 onToggle 함수를 TodoListItem에 전달

--- src/TodoListList.js
+++ src/TodoListList.js
@@ -1,12 +1,12 @@
 import './TodoListList.css';
 import TodoListItem from './TodoListItem';
 
-const TodoListList = ({todos, onRemove}) => {
+const TodoListList = ({todos, onRemove, onToggle}) => {
     // ↑ TodoList에서 받아온 todos와 onRemove
     return(
         <div className="TodoListList">
             {todos.map(todo => (
-                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove}/>
+                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>
             ))}
         </div>
     );
