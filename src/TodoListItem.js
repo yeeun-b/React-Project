5fd701ba1e6599a833f278c124b1975bfabb55f1
yeeun-b <yeeun.park.417@gmail.com>
2022-09-13 09:13:35 +0900

    0913 주석 추가, 수정 버튼 추가

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,4 +1,4 @@
-import { MdCheckBox, MdCheckBoxOutlineBlank, MdRemoveCircleOutline } from "react-icons/md";
+import { MdCheckBox, MdCheckBoxOutlineBlank, MdRemoveCircleOutline, MdModeEditOutline } from "react-icons/md";
 import './TodoListItem.scss';
 import cn from 'classnames';
 
@@ -7,10 +7,18 @@ const TodoListItem = ({todo, onRemove, onToggle}) => {
 
     return(
         <div className="TodoListItem">
+            {/* checked가 true일 때 checked라는 클래스 추가 */}
             <div className={cn('checkbox', {checked})} onClick={() => onToggle(id)}>
+                {/* checked가 true면 체크된 아이콘이 / false면 체크되지 않은 아이콘 출력 */}
                 {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
                 <div className="text">{text}</div>
             </div>
+            {/* 수정 버튼 */}
+            <div className="edit">
+                {/* 클릭 시 id를 기준으로 항목 삭제하는 함수 호출 */}
+                <MdModeEditOutline />
+            </div>
+            {/* 삭제 버튼 */}
             <div className="remove" onClick={() => onRemove(id)}>
                 {/* 클릭 시 id를 기준으로 항목 삭제하는 함수 호출 */}
                 <MdRemoveCircleOutline />
