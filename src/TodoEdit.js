2013bce526f42cf9d7657f5c213a4ffe00588d56
yeeun-b <yeeun.park.417@gmail.com>
2022-09-13 09:26:28 +0900

    0913 TodoEdit 기능 작성

--- src/TodoEdit.js
+++ src/TodoEdit.js
@@ -1,9 +1,27 @@
 import './TodoEdit.scss';
 
 const TodoEdit = ({selectedTodo, onUpdated}) => {
+    const [value, setValue] = useState('');
+    const onChange = useCallback((e) => {
+        setValue(e.target.value);
+    }, []);
+    const onSubmit = useCallback((e) => {
+        setValue(''); // value 초기화
+        // 새로고침 방지
+        e.preventDefault();
+    },[value],);
+    
     return(
-        <div>
-
+        <div className="EditBacktround">
+            <form onSubmit={onSubmit} className="todoEditInsert">
+                <h2>수정하기</h2>
+                <input
+                    onChange={onChange}
+                    value={value}
+                    placeholder="할 일 수정하기"
+                ></input>
+                <button type="submit">수정하기</button>
+            </form>
         </div>
     );
 };
