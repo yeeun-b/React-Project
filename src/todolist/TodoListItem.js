1b8e909571a18212d6aa2e5b172e7ece16e0b6a9
yeeun-b <yeeun.park.417@gmail.com>
2022-10-08 00:04:54 +0900

    1008 체크박스 설정 변경

--- src/todolist/TodoListItem.js
+++ src/todolist/TodoListItem.js
@@ -30,7 +30,7 @@ const TodoListItem = ({todo, onRemove, onToggle, handleSingleCheck, onInsertTogg
                 onChange={(e) => handleSingleCheck(e.target.checked, id)}
                 // 체크된 아이템 배열에 해당 아이템이 있을 경우 선택 활성화, 아닐 시 해제
                 // checked={checkItems.includes(id) ? true : false}
-                />
+                className='deletbox'/>
         </div>
     );
 };
